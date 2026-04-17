d453d14a858dc37c527a0f71fc0bb1bff8f23876
Ангелина Петрова <linalleks@gmail.com>
2026-04-17 19:57:53 +0700

    added README.md

--- main.py
+++ main.py
@@ -12,9 +12,3 @@ if __name__ == '__main__':
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
     active_passcards = Passcard.objects.filter(is_active=True)
     print('Количество активных пропусков:', len(active_passcards))
-
-    # some_passcard = passcards[0]
-    # print(f"owner_name: {some_passcard.owner_name}\n"
-    #       f"passcode: {some_passcard.passcode}\n"
-    #       f"created_at: {some_passcard.created_at}\n"
-    #       f"is_active: {some_passcard.is_active}")
